db7f3c11dc22f7761eec02fbc8ccf7d324ea87d1
aditirajak131106 <aditi.rajak.batch2024@sitnagpur.siu.edu.in>
2026-01-22 10:30:49 +0530

    Experiment 3
    i have learned to use nano editor and written a basic operation code in c.

---
+++ experiment 3(1).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int a = 4;
+	int b = 9;
+	int sum = a + b;
+	printf("sum: %d\n",sum);
+	int diff = a - b;
+	printf("Sub: %d\n",diff);
+	int mul = a*b;
+	printf("product: %d\n",mul);
+	int div = a/b;
+	printf("division: %d\n",div);
+}
